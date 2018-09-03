0120869fe06f04d5481f7f426ce3cbc078953afd
Viktoriya Hnativ <vita96.ua@gmail.com>
2018-09-03 11:55:27 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const request = 'Введіть рівняння';
+ alert(productName);
+ var a;
+ var b;
+ var c;
+function solveQuadr(a, b, c) {
+    var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    return result + "<br>" + result2;
+const result solveQuadr(a, b, c);
+alert(result);
